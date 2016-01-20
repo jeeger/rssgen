35998bf076b4cbecc6d8fe343847da62764af274
Jan Seeger <jan.seeger@thenybble.de>
2016-01-20 11:05:36 +0100

    Reworked error handling.
    Now rewrapping all errors into a common error type (with a string
    error). Got rid of all uses of fromJust and replaced with error monad.

--- main.hs
+++ main.hs
@@ -6,6 +6,7 @@ import Data.Maybe
 import Data.List
 import Data.ConfigFile
 import Text.ParserCombinators.Parsec.Error
+import Text.ParserCombinators.Parsec.Pos
 import Control.Lens
 import Control.Monad.Except
 import Control.Monad
@@ -32,7 +33,8 @@ $(makeLenses ''Config)
 
 
 type RSSgenError = Either (CPErrorData, String) Text.ParserCombinators.Parsec.Error.ParseError
-type RSSgenErrorIO = ExceptT RSSgenError IO
+type MyError = String
+type RSSgenErrorIO = ExceptT MyError IO
 
 readConfig:: FilePath -> ExceptT (CPErrorData, String) IO Config
 readConfig p = do cp <- join $ liftIO $ readfile emptyCP p
@@ -73,22 +75,26 @@ filterAndRemoveRSSToken =
   map (over description (replace rsstoken ""))
   . filter (views description (isInfixOf rsstoken))
 
-generateRSS:: Config -> LogEntries -> RSS
-generateRSS config entries = RSS
-  (view rssTitle config)
-  (views rssLink (fromJust . parseURI) config)
-  (view rssDescription config)
-  [ (views rssCopyright Copyright config), Generator $ "rssgen-" ++ (showVersion version)]
-  (map (processEntry config) entries)
+
+-- TODO: Parse URI when reading configuration.
+generateRSS:: Config -> LogEntries -> Except MyError RSS 
+generateRSS config entries = do processed <- mapM (processEntry config) entries
+                                mainlink <- ExceptT . return $ maybe (Left "Failed to parse URL") Right (parseURI (view rssLink config))
+                                return $ RSS
+                                  (view rssTitle config)
+                                  mainlink
+                                  (view rssDescription config)
+                                  [ (views rssCopyright Copyright config), Generator $ "rssgen-" ++ (showVersion version)]
+                                  processed
 
 -- Find the file with the most added changes, if none, find with most deleted. That is probably
 -- the file with the relevant changes, and so we generate a link from that. Failing that, we just make a link to the
 -- front page.
-generateLink:: Config -> LogEntry -> URI
+generateLink:: Config -> LogEntry -> Maybe URI
 generateLink cfg entry = generateURL cfg (view filechanges entry)
 
 -- TODO: Make front page link configurable.
-generateURL:: Config -> Maybe FileChanges -> URI
+generateURL:: Config -> Maybe FileChanges -> Maybe URI
 generateURL cfg = substitute_link cfg . find_most_edited cfg
   where find_most_edited cfg (Just changes) = let (name, _, _) = foldl maximum_changed_file ("", 0, 0) changes
                                        in name
@@ -106,26 +112,47 @@ generateURL cfg = substitute_link cfg . find_most_edited cfg
         -- Replace source extension with target extension, delete source path, prepend destination URL,
         -- parse into URI
         substitute_link config =
-          fromJust .
           parseURI .
           ((view fileDestURL cfg) ++) .
           (replace (view fileSourcePath config) "") .
           (replace (view fromExtension config) (view toExtension config))
 
-processEntry:: Config -> LogEntry -> Item
-processEntry config entry = [ Title (view title entry),
-                              Description (view description entry),
-                              Author (view author entry),
-                              PubDate (view date entry),
-                              Link (generateLink config entry)
-                            ]
+processEntry:: Config -> LogEntry -> Except MyError Item
+processEntry config entry = case generateLink config entry of
+  Just link -> 
+    return [ Title (view title entry),
+             Description (view description entry),
+             Author (view author entry),
+             PubDate (view date entry),
+             Link link
+           ]
+  Nothing ->
+    throwError ("Failed to create URL from log entry " ++ show entry)
+
+formatConfigError:: (CPErrorData, String) -> String
+formatConfigError (edata, string) = case edata of
+  ParseError s -> "Error parsing configuration: " ++ s
+  SectionAlreadyExists s -> "Error in config. Section already exists: " ++ s
+  NoSection s -> "Error in configuration. No such section " ++ s
+  NoOption s -> "Error in configuration. No such option " ++ s
+  OtherProblem s -> "Miscellaneous error in configuration: " ++ s
+  ++ "at " ++ string
+
+formatParseError:: ParseError -> String
+formatParseError pe = "Parse error: " ++ (unlines . map showMessage $ errorMessages pe) ++ " at " ++ showPos (errorPos pe)
+  where showMessage msg = case msg of
+          SysUnExpect s -> "Unexpected input " ++ s
+          UnExpect s -> "Unexpected input " ++ s
+          Expect s -> "Expected input " ++ s
+          Message s -> "General parsing error " ++ s
+        showPos pos = "line " ++ show (sourceLine pos) ++ " column " ++ show (sourceColumn pos)
 
 runMain:: String -> RSSgenErrorIO ()
-runMain confpath = do conf <- withExceptT (\e -> Left e) $ readConfig confpath
+runMain confpath = do conf <- withExceptT formatConfigError $ readConfig confpath
                       lines <- liftIO getContents
-                      entries <- withExceptT (\e -> Right e) $ ExceptT $ return $ parseLog lines "standard input"
-                      result <- return $ generateRSS conf (filterAndRemoveRSSToken entries)
-                      liftIO $ print $ showXML $ rssToXML result
+                      entries <- withExceptT formatParseError . ExceptT . return $ parseLog  lines "standard input"
+                      result <- ExceptT . return . runExcept . generateRSS conf $ filterAndRemoveRSSToken entries
+                      liftIO . print . showXML . rssToXML $ result
                       return ()
                       
 main = do result <- runExceptT $ runMain "rssgen.conf"
